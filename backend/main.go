305f6e3ebd32f10c60baf8f83b0a5bbc2b2f7b5f
Stephen Rhodes <srhodes@bmt.world>
2022-05-20 23:59:10 -0400

    fix: test2 [CYN-35]

--- backend/main.go
+++ backend/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	box := packr.New("site", "../frontend/dist")
-	fmt.Println("hello!2")
+	fmt.Println("hello!22")
 	http.Handle("/", http.FileServer(box))
 
 	http.ListenAndServe(":3030", nil)
